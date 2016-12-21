d05a733c1906bcd8f83e68ad29b7e094af8a2ec9
limor-gs <limor@gigaspaces.com>
2016-12-21 17:27:40 +0200

    new versioning

--- packages-urls/provision.sh
+++ packages-urls/provision.sh
@@ -2,7 +2,6 @@
 
 export VERSION="16.12.1"
 export PRERELEASE="community"
-export CORE_TAG_NAME="master"
 export AWS_S3_BUCKET="gigaspaces-repository-eu"
 export AWS_S3_PATH="cloudify/${VERSION}/build"
 
