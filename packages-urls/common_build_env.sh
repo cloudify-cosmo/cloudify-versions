eb0e2ed666e3d25696fdb95df70af35d3ccfb4f9
limor-gs <limor@gigaspaces.com>
2017-03-22 15:40:03 +0200

    Update common_build_env.sh

--- packages-urls/common_build_env.sh
+++ packages-urls/common_build_env.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-export VERSION="16.12.1"
+export VERSION="17.3.31"
 export PRERELEASE="community"
 export AWS_S3_BUCKET="gigaspaces-repository-eu"
 export AWS_S3_PATH="cloudify/${VERSION}/build"
-
