946c2727d45ef9f53d7bafdbca62aee7e4b6a71d
opencm <opencm@gigaspaces.com>
2017-09-12 14:41:24 +0300

    CFY-7243 Upload all cloudify packages to a new S3 bucket

--- packages-urls/common_build_env.sh
+++ packages-urls/common_build_env.sh
@@ -2,5 +2,5 @@
 
 export VERSION="17.9.21"
 export PRERELEASE="community"
-export AWS_S3_BUCKET="gigaspaces-repository-eu"
+export AWS_S3_BUCKET="cloudify-release-eu"
 export AWS_S3_PATH="cloudify/${VERSION}/build"
