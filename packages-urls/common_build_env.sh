9a4364adb724fb069a3ab476e3d582282c886326
opencm <limor@gigaspaces.com>
2019-09-11 16:51:54 +0300

    Bump Version

--- packages-urls/common_build_env.sh
+++ packages-urls/common_build_env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export VERSION="19.08.08"
+export VERSION="19.09.26"
 export PRERELEASE="community"
 export AWS_S3_BUCKET="cloudify-release-eu"
 export AWS_S3_PATH="cloudify/${VERSION}/build"
