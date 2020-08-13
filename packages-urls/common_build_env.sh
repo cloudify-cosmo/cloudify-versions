280f345c9d54a476a5c889d8637b26ea99297f76
Alex Molev <amolev@gmail.com>
2020-08-14 01:17:36 +0300

    Updating to version 20.02.23

--- packages-urls/common_build_env.sh
+++ packages-urls/common_build_env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export VERSION="19.11.10"
+export VERSION="20.02.23"
 export PRERELEASE="community"
 export AWS_S3_BUCKET="cloudify-release-eu"
 export AWS_S3_PATH="cloudify/${VERSION}/build"
